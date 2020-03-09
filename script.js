e1f33e06487e0a08542f0cc49404157c42fe9163
cristian gil <cristianingeniero2018@hotmail.com>
2020-03-08 20:32:16 -0500

    inicializar jquery, manejar elementos del dom y css

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert('tutorial jquery');
+//crear un elemento y posicionarlo dentro de un elemento con el id especificado
+$(function () {
+    var a = $('<a>',{
+        href: 'http//platzi.com',
+        target: '_blank',
+        html : 'Ir a platzi'
+    })
+    $('#app-body').append(a);
+    //a.attr('href'); -> get del objeto a
+    
+})
+
+$(function(){
+    $('<p>',{html: 'Me acaban de crear'}).appendTo($('header#app-header'))
+})
+
+//en jquery se manejan los objects jquery, a un objeto por convención se le antepone el signo $, var $variable
+
+//manipular los estilos css
+
+$(function(){
+    $('h1').css({
+        'font-size':'30px'
+    })
+})
+
